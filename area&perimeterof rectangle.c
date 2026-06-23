3973c7f8560fbdbe6989414337bb182afcf07467
sagardalvi18 <sagardalvi2122@gmail.com>
2026-06-23 19:51:33 +0530

    Add files via upload

---
+++ area&perimeterof rectangle.c
@@ -0,0 +1,23 @@
+ll// Experiment No 2
+// Name-Sagar Ravindra Dalvi
+//PRN- 2503033111372L007
+#include <stdio.h>
+
+int main() {
+    float length, width, area, perimeter;
+
+    printf("Enter length: ");
+    scanf("%f", &length);
+
+    printf("Enter width: ");
+    scanf("%f", &width);
+
+    area = length * width;
+    perimeter = 2 * (length + width);
+
+    printf("Area of Rectangle = %.2f\n", area);
+    printf("Perimeter of Rectangle = %.2f\n", 
+    perimeter);
+
+    return 0;
+}
